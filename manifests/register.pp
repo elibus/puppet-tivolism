cf38354d7b794afbca7e88e414c6e360d899e1c0
Marco Tizzoni <marco.tizzoni@gmail.com>
2014-10-30 16:29:49 +0100

    indent

--- manifests/register.pp
+++ manifests/register.pp
@@ -1,14 +1,14 @@
-  # used by other modules to register themselves
-  define tivolism::register($content='', $order='10') {
-    if $content == '' {
-      $body = $name
-    } else {
-      $body = $content
-    }
-
-    concat::fragment{ "dsm.sys fragment_${name}":
-      target  => '/opt/tivoli/tsm/client/ba/bin/dsm.sys',
-      order   => $order,
-      content => "${body}\n"
-    }
+# used by other modules to register themselves
+define tivolism::register($content='', $order='10') {
+  if $content == '' {
+    $body = $name
+  } else {
+    $body = $content
   }
+
+  concat::fragment{ "dsm.sys fragment_${name}":
+    target  => '/opt/tivoli/tsm/client/ba/bin/dsm.sys',
+    order   => $order,
+    content => "${body}\n"
+  }
+}
